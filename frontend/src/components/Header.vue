8580933dad06a1367485a824d40afe3f64c75e55
maarikamarkus <maarika.markus@gmail.com>
2020-08-04 20:48:32 +0300

    header to grid css

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,9 @@
 <template lang="pug">
-    div
-        .date
-            #dayOfMonth {{dayOfMonth}}
-            #secondColumn
-                #month {{month}}
-                #year {{year}}
-            #dayOfWeek {{dayOfWeek}}
+    .date
+        #dayOfMonth {{dayOfMonth}}
+        #month {{month}}
+        #year {{year}}
+        #dayOfWeek {{dayOfWeek}}
 </template>
 
 <script>
@@ -25,27 +23,44 @@ export default {
 
 <style>
 .date {
-    height: 50px;
+    display: grid;
+    grid-template-columns: 20px 70px auto 100px;
+    grid-template-rows: repeat(2, auto);
+    column-gap: 11px;
+    row-gap: 3px;
     text-transform: uppercase;
     font-family: 'Roboto', sans-serif;
+    justify-content: stretch;
 }
 
-#dayOfMonth, #dayOfWeek, #secondColumn {
-    display: inline-block;
+#dayOfMonth, #dayOfWeek {
+    align-self: center;
 }
 
 #dayOfMonth {
+    grid-column: 1;
+    grid-row: 1 / span 2;
     font-size: 41px;
-    height: 100%;
-    margin-right: 7px;
 }
 
 #month {
     font-size: 13px;
+    grid-column: 2;
+    grid-row: 1;
+    align-self: end;
+}
+
+#year {
+    font-size: 15px;
+    grid-column: 2;
+    grid-row: 2;
+    align-self: start;
 }
 
 #dayOfWeek {
-    margin-top: 13px;
-    float: right;
+    font-size: 17px;
+    grid-column: 4;
+    grid-row: 1 / span 2;
+    justify-self: end;
 }
 </style>
